68fc58108693a64f772bd806b756d7f23c53ee90
Marie Brown <mariebrown.mlb@gmail.com>
2024-05-11 20:07:19 +0100

    Main class setup

--- src/LevelDesigner.java
+++ src/LevelDesigner.java
@@ -6,4 +6,32 @@ public class LevelDesigner extends PApplet {
         LevelDesigner app = new LevelDesigner();
         PApplet.runSketch(processingArgs, app);
     }
+
+    @Override   
+    public void settings(){
+        // runs once before the processing sketch has been set up
+
+        size(1920, 1080); // set initial window size
+        noSmooth(); // turns off interpolation, must be run after size
+    }
+
+    @Override
+    public void setup(){
+        // runs once after the processing sketch has been set up
+    }
+
+    @Override
+    public void draw(){
+        // called every frame
+        update();
+        render();
+    }
+
+    private void update(){
+        //stub
+    }
+
+    private void render(){
+        //stub
+    }
 }
