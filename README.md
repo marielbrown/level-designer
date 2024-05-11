2e2ea88ef0aeade722ab2e9141653117b1d4bd03
Marie Brown <mariebrown.mlb@gmail.com>
2024-05-11 19:48:47 +0100

    Create processing appication

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # level-designer
-A level designer tool for aiding the development of games in java using the Processing library
+A level designer tool for aiding the development of games in Java using the Processing library
---
+++ src/LevelDesigner.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class LevelDesigner extends PApplet {
+    public static void main(String[] args){
+        String[] processingArgs = {"Level Designer"};
+        LevelDesigner app = new LevelDesigner();
+        PApplet.runSketch(processingArgs, app);
+    }
+}
